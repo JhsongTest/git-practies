e1051865c45bb903a36253739194f9336c9c827e
JhsongTest <nn0221@naver.com>
2023-05-01 11:32:22 +0900

    modified

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 def print_hello():
 	animals = ['dog', 'cat', 'hamster', 'tiger']
 	foods = ['chicken', 'pork', 'beef', 'pasta', 'bibimbob']
-	names = ['John', 'Jane', 'Gil-dong','Dong-eun',] # with trailing 
-comma
+	names = ['John', 'Jane', 'Gil-dong','Dong-eun',]
+	# with trailing comma
 
 	for f_name in names:
 		print(f'hello, {f_name}')
